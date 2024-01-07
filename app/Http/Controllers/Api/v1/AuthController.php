a046f4ac7acd3d1ba983740579050929456f7bde
bayupriyambada17 <bayupm.dev@gmail.com>
2024-01-07 13:28:43 +0700

    first commit

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = auth()->guard('pos')->attempt($credentials)) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Email or Password is incorrect'
+            ], 401);
+        }
+
+        return response()->json([
+            'success' => true,
+            'user'    => auth()->guard('pos')->user(),
+            'token'   => $token
+        ], 200);
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string', new Password(8, true),
+            'role' => 'required|in:admin,owner,kasir,manager'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role
+        ]);
+
+        return response()->json($user, 201);
+    }
+
+    public function getUser()
+    {
+        //response data "user" yang sedang login
+        return response()->json([
+            'success' => true,
+            'user'    => auth()->guard('pos')->user()
+        ], 200);
+    }
+
+    /**
+     * refreshToken
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function refreshToken(Request $request)
+    {
+        //refresh "token"
+        $refreshToken = JWTAuth::refresh(JWTAuth::getToken());
+
+        //set user dengan "token" baru
+        $user = JWTAuth::setToken($refreshToken)->toUser();
+
+        //set header "Authorization" dengan type Bearer + "token" baru
+        $request->headers->set('Authorization', 'Bearer ' . $refreshToken);
+
+        //response data "user" dengan "token" baru
+        return response()->json([
+            'success' => true,
+            'user'    => $user,
+            'token'   => $refreshToken,
+        ], 200);
+    }
+
+    /**
+     * logout
+     *
+     * @return void
+     */
+    public function logout()
+    {
+        //remove "token" JWT
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        //response "success" logout
+        return response()->json([
+            'success' => true,
+        ], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::prefix('v1')->group(function () {
+    Route::post('/auth/register', [AuthController::class, 'register']);
+    Route::post('/auth/login', [AuthController::class, 'login']);
+    Route::middleware(['auth:pos'])->group(function () {
+        Route::get('/me', [AuthController::class, 'getUser']);
+        Route::get('/refresh', [AuthController::class, 'refreshToken']);
+        Route::get('/logout', [AuthController::class, 'logout']);
+    });
+});
